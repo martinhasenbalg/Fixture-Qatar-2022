1b7fb4e687a521f935ff9e68fb62ecf209fd2d27
Martin Hasenbalg <85136787+martinhasenbalg@users.noreply.github.com>
2022-11-20 15:01:43 -0300

    Actualizar grupoa.js

--- grupoa.js
+++ grupoa.js
@@ -2,23 +2,23 @@ soccer.addEquipo(['QATAR', 'ECUADOR', 'SENEGAL', 'PAISES BAJOS']);
     
 soccer.updateEquipo([{
         nombre: 'QATAR',
-        jugados: 0,
+        jugados: 1,
         ganados: 0,
         empates: 0,
-        perdidos: 0,
+        perdidos: 1,
         golesfavor: 0,
-        golescontra: 0,
+        golescontra: 2,
         puntos: 0
     },
     {
         nombre: 'ECUADOR',
-        jugados: 0,
-        ganados: 0,
+        jugados: 1,
+        ganados: 1,
         empates: 0,
         perdidos: 0,
-        golesfavor: 0,
+        golesfavor: 2,
         golescontra: 0,
-        puntos: 0
+        puntos: 3
         },
     {
         nombre: 'SENEGAL',
