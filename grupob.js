eaaac6fadabbdcffc0722f8e47d5aa4b56094196
martinhasenbalg <martinhasen2@yahoo.com.ar>
2022-11-21 11:53:49 -0300

    ING-IRN

--- grupob.js
+++ grupob.js
@@ -2,22 +2,22 @@ soccer.addEquipo(['INGLATERRA', 'IRAN', 'ESTADOS UNIDOS', 'GALES']);
     
 soccer.updateEquipo([{
         nombre: 'INGLATERRA',
-        jugados: 0,
-        ganados: 0,
+        jugados: 1,
+        ganados: 1,
         empates: 0,
         perdidos: 0,
-        golesfavor: 0,
-        golescontra: 0,
+        golesfavor: 5,
+        golescontra: 1,
         puntos: 0
     },
     {
         nombre: 'IRAN',
-        jugados: 0,
+        jugados: 1,
         ganados: 0,
         empates: 0,
-        perdidos: 0,
-        golesfavor: 0,
-        golescontra: 0,
+        perdidos: 1,
+        golesfavor: 1,
+        golescontra: 5,
         puntos: 0
         },
     {
