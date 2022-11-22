462c4527808e8a2d98f7bfd61819606fcbee0294
martinhasenbalg <martinhasen2@yahoo.com.ar>
2022-11-22 09:18:59 -0300

    ar-as

--- grupoc.js
+++ grupoc.js
@@ -2,23 +2,23 @@ soccer.addEquipo(['ARGENTINA', 'ARABIA SAUDITA', 'MEXICO', 'POLONIA']);
     
 soccer.updateEquipo([{
         nombre: 'ARGENTINA',
-        jugados: 0,
+        jugados: 1,
         ganados: 0,
         empates: 0,
-        perdidos: 0,
-        golesfavor: 0,
-        golescontra: 0,
+        perdidos: 1,
+        golesfavor: 1,
+        golescontra: 2,
         puntos: 0
     },
     {
         nombre: 'ARABIA SAUDITA',
-        jugados: 0,
-        ganados: 0,
+        jugados: 1,
+        ganados: 1,
         empates: 0,
         perdidos: 0,
-        golesfavor: 0,
-        golescontra: 0,
-        puntos: 0
+        golesfavor: 2,
+        golescontra: 1,
+        puntos: 3
         },
     {
         nombre: 'MEXICO',
