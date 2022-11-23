4812395712cae51b5d4c6fe3d2c2ff19197efd30
martinhasenbalg <martinhasen2@yahoo.com.ar>
2022-11-23 12:14:14 -0300

    ale/jpn

--- grupoe.js
+++ grupoe.js
@@ -22,23 +22,23 @@ soccer.updateEquipo([{
         },
     {
         nombre: 'ALEMANIA',
-        jugados: 0,
+        jugados: 1,
         ganados: 0,
         empates: 0,
-        perdidos: 0,
-        golesfavor: 0,
-        golescontra: 0,
+        perdidos: 1,
+        golesfavor: 1,
+        golescontra: 2,
         puntos: 0
     },
     {
         nombre: 'JAPON',
-        jugados: 0,
-        ganados: 0,
+        jugados: 1,
+        ganados: 1,
         empates: 0,
         perdidos: 0,
-        golesfavor: 0,
-        golescontra: 0,
-        puntos: 0    
+        golesfavor: 2,
+        golescontra: 1,
+        puntos: 3    
     },
     ]);
         
