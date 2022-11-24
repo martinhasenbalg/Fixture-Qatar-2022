1a8f0b68ea50b478755513d3c307771418f7808b
martinhasenbalg <martinhasen2@yahoo.com.ar>
2022-11-24 15:29:35 -0300

    pt-gh

--- grupoh.js
+++ grupoh.js
@@ -2,22 +2,22 @@ soccer.addEquipo(['PORTUGAL', 'GHANA', 'URUGUAY', 'COREA DEL SUR']);
     
 soccer.updateEquipo([{
         nombre: 'PORTUGAL',
-        jugados: 0,
-        ganados: 0,
+        jugados: 1,
+        ganados: 1,
         empates: 0,
         perdidos: 0,
-        golesfavor: 0,
-        golescontra: 0,
-        puntos: 0
+        golesfavor: 3,
+        golescontra: 2,
+        puntos: 3
     },
     {
         nombre: 'GHANA',
-        jugados: 0,
+        jugados: 1,
         ganados: 0,
         empates: 0,
-        perdidos: 0,
-        golesfavor: 0,
-        golescontra: 0,
+        perdidos: 1,
+        golesfavor: 2,
+        golescontra: 3,
         puntos: 0
         },
     {
