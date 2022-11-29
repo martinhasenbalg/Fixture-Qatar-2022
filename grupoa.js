6e5b194e30635a5a4d46448c745731daea8e73ab
martinhasenbalg <martinhasen2@yahoo.com.ar>
2022-11-29 14:11:27 -0300

    grupoa

--- grupoa.js
+++ grupoa.js
@@ -2,43 +2,43 @@ soccer.addEquipo(['QATAR', 'ECUADOR', 'SENEGAL', 'PAISES BAJOS']);
     
 soccer.updateEquipo([{
         nombre: 'QATAR',
-        jugados: 2,
+        jugados: 3,
         ganados: 0,
         empates: 0,
-        perdidos: 2,
+        perdidos: 3,
         golesfavor: 1,
-        golescontra: 5,
+        golescontra: 7,
         puntos: 0
     },
     {
         nombre: 'ECUADOR',
-        jugados: 2,
+        jugados: 3,
         ganados: 1,
         empates: 1,
-        perdidos: 0,
-        golesfavor: 3,
-        golescontra: 1,
+        perdidos: 1,
+        golesfavor: 4,
+        golescontra: 3,
         puntos: 4
         },
     {
         nombre: 'SENEGAL',
-        jugados: 2,
-        ganados: 1,
+        jugados: 3,
+        ganados: 2,
         empates: 0,
         perdidos: 1,
-        golesfavor: 3,
-        golescontra: 2,
-        puntos: 3
+        golesfavor: 5,
+        golescontra: 3,
+        puntos: 6
     },
     {
         nombre: 'PAISES BAJOS',
-        jugados: 2,
-        ganados: 1,
+        jugados: 3,
+        ganados: 2,
         empates: 1,
         perdidos: 0,
-        golesfavor: 3,
+        golesfavor: 5,
         golescontra: 1,
-        puntos: 4    
+        puntos: 7    
     },
     ]);
         
