28b289d80a491cab79512f77b6f36962e0b2da7e
martinhasenbalg <martinhasen2@yahoo.com.ar>
2022-12-01 18:30:34 -0300

    01-12-3

--- grupoe.js
+++ grupoe.js
@@ -2,43 +2,43 @@ soccer.addEquipo(['ESPAÑA', 'COSTA RICA', 'ALEMANIA', 'JAPON']);
     
 soccer.updateEquipo([{
         nombre: 'ESPAÑA',
-        jugados: 2,
+        jugados: 3,
         ganados: 1,
         empates: 1,
-        perdidos: 0,
-        golesfavor: 8,
-        golescontra: 1,
+        perdidos: 1,
+        golesfavor: 9,
+        golescontra: 3,
         puntos: 4
     },
     {
         nombre: 'COSTA RICA',
-        jugados: 2,
+        jugados: 3,
         ganados: 1,
         empates: 0,
-        perdidos: 1,
-        golesfavor: 1,
-        golescontra: 7,
+        perdidos: 2,
+        golesfavor: 3,
+        golescontra: 11,
         puntos: 3
         },
     {
         nombre: 'ALEMANIA',
-        jugados: 2,
-        ganados: 0,
+        jugados: 3,
+        ganados: 1,
         empates: 1,
         perdidos: 1,
-        golesfavor: 2,
-        golescontra: 3,
-        puntos: 1
+        golesfavor: 6,
+        golescontra: 5,
+        puntos: 4
     },
     {
         nombre: 'JAPON',
-        jugados: 2,
-        ganados: 1,
+        jugados: 3,
+        ganados: 2,
         empates: 0,
         perdidos: 1,
-        golesfavor: 2,
-        golescontra: 2,
-        puntos: 3    
+        golesfavor: 4,
+        golescontra: 3,
+        puntos: 6    
     },
     ]);
         
